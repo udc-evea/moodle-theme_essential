be5563a0b732de6bc446d50602f10a3be869766e
Nicolas H. Fernandez <nicof05@gmail.com>
2014-09-30 16:50:27 -0300

    Arreglados cambios en el chat, pasa a color verdecuando hay nuevos mjes. Falta terminar scrollear al ingresar un mje.

--- layout/includes/chat.php
+++ layout/includes/chat.php
@@ -7,7 +7,7 @@
     ?>
     <div id="caja"> <!-- caja contenedora del menu + chat + form -->
         <div id="chatMenu"> <!-- caja contenedora del MENU del chat -->
-            <p class="welcome" id="botonOcultaChat"><b><?php echo $apeNomUser ?></b><i id="iconoChat" class="icon icon-minus icon-white"></i></p>
+            <p class="welcome" id="botonOcultaChat"><b><?php echo $apeNomUser ?></b><i id="iconoChat" class="icon icon-chevron-up icon-white"></i></p>
             <!--<button id="botonOcultaChat" name="Expandir/Bajar chat"></button> -->
         </div>
         <div id="chatContenido"> <!-- caja contenedora del chat + form -->
@@ -38,6 +38,7 @@
     <script>
         //funcion para ocultar la ventana de chat 
         //oculto la ventana del chat
+        var abajo = true;
         var urlActual = document.location;
         var urlActual = String(urlActual);
         var cant = urlActual.length;
@@ -46,16 +47,15 @@
             $( "#chatContenido" ).slideToggle(800); //mas lento
         }else{
             $( "#chatContenido" ).slideToggle(8);//mas rapido
-        }
+        }                       
         // si clickeo el boton se oculta/muestra el chat
         $( "#botonOcultaChat" ).click(function() {
             $( "#chatContenido" ).slideToggle(800);
-        });        
-
-        $.parpadearChat = function(color){
-            $("#botonOcultaChat").css("background-color", color);
-        };
-
+        });
+                
+        //variable utilizada para determinar el tamaño del log.html
+        var tamanio = 0;
+        
         //If user submits the form
         $("#submitmsg").click(function(){
             var clientmsg = $("#usermsg").val();
@@ -73,17 +73,18 @@
             //$("#usermsg").attr("value", "");
             $("#usermsg").val('');
             
-            //$.parpadearChat("#E3FDFF");
-            //setTimeout(parpadearChat("#E3FDFF"),1000); //color mas claro
-            //setTimeout(parpadearChat("#019DEB"),1000); //color base
-            
             return false;
         });
         
+        var scrolleo = true;
+        
         //Load the file containing the chat log
         function loadLog(){
+            var newTamanio = $("#chatbox").html().length;
             //var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 5; //Scroll height before the request
             var oldscrollHeight = $("#chatbox").scrollTop();
+            var newscrollHeight = $("#chatbox").prop("scrollHeight") - 20; //Scroll height after the request
+            
             var urlFinal = "../theme/essential/layout/includes/log.html";
             var urlActual = document.location;
             var urlActual = String(urlActual);
@@ -96,18 +97,44 @@
                 url: urlFinal,//url: "theme/essential/layout/includes/log.html",
                 cache: false,
                 success: function(html){
-                    $("#chatbox").html(html); //Insert chat log into the #chatbox div
+                    $("#chatbox").html(html); //Insert chat log into the #chatbox div 
+                    //alert("scrolleo: "+scrolleo);
                         //Auto-scroll 
-                        var newscrollHeight = $("#chatbox").prop("scrollHeight") - 20; //Scroll height after the request
-                        if(newscrollHeight > oldscrollHeight){
+                        //var newscrollHeight = $("#chatbox").prop("scrollHeight") - 20; //Scroll height after the request
+                        if((newscrollHeight > oldscrollHeight)&&(scrolleo)){
                             $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-                        }               
+                            scrolleo = false;
+                            //alert("scrolleo: "+scrolleo);
+                        }
                     }
-                });
-                
-                $.parpadearChat("#019DEB");
-            }            
-        //Defino el intervalo de actualiación del chat
-        setInterval (loadLog, 2500);    //Reload file every 2500 ms or x ms if you wish to change the second parameter
+             });
+             //alert("Antes del If= tamanio: "+tamanio+" - NewTamanio: "+newTamanio);
+             //compruebo si hay mensajes nuevos, y si hay cambio el color de la ventana a verde.
+             var diff = Math.abs(newTamanio - tamanio);
+             var multiplode35 = ((diff % 35) === 0);
+             //alert("multiplo: "+multiplode35);
+             if((newTamanio > tamanio) && (tamanio !== 0) && (tamanio !== 29) &&(!multiplode35)){
+                 $("#caja").css("border","3px solid green");
+                 $("#chatMenu").css("background-color","green");
+             }
+             tamanio = newTamanio;
+             //alert("Despues del If = tamanio: "+tamanio+" - NewTamanio: "+newTamanio);
+             //alert("tamOld: "+tamanio+" - NewTam: "+newTamanio);
+        }
+        //Defino el intervalo de actualiación del chat (1 segundo y medio)
+        setInterval (loadLog, 1500);    //Reload file every 2500 ms or x ms if you wish to change the second parameter
+        //cuando hacen click en el chat se pone el color azul base de siempre
+        $("#caja").click(function(){
+            $("#caja").css("border","3px solid #019DEB");
+            $("#chatMenu").css("background-color","#019DEB");
+        })
+        $("#chatContenido").click(function(){
+            $("#caja").css("border","3px solid #019DEB");
+            $("#chatMenu").css("background-color","#019DEB");
+        })
+        $("#chatbox").click(function(){
+            $("#caja").css("border","3px solid #019DEB");
+            $("#chatMenu").css("background-color","#019DEB");
+        })
     </script>
 <?php endif; ?>
